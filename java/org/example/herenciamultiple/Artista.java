48cfa83626a1071157c4747269a7e74f7bdd3335
Carlos Pineda <125937872+PinedaBellako@users.noreply.github.com>
2023-04-23 16:44:38 -0600

    Delete java/org/example directory

--- java/org/example/herenciamultiple/Artista.java
+++
@@ -1,6 +0,0 @@
-package org.example.herenciamultiple;
-
-public interface Artista {
-    public int ensayar(String diciplina);
-    public boolean presentarObra();
-}
--- java/org/example/herenciamultiple/Deportista.java
+++
@@ -1,6 +0,0 @@
-package org.example.herenciamultiple;
-
-public interface Deportista {
-    public abstract int entrenar();
-    public abstract boolean presentarCompetencia(String ubicacion);
-}
