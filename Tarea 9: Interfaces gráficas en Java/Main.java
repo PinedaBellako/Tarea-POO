aecf0336503ef0bbc78d29f5bb6366240b754af7
Carlos Pineda <125937872+PinedaBellako@users.noreply.github.com>
2023-05-02 15:34:19 -0600

    Add files via upload

---
+++ Tarea 9: Interfaces gráficas en Java/Main.java
@@ -0,0 +1,8 @@
+package org.example.tarea9;
+
+import org.example.tarea9.iu.Principal;
+public class Main {
+    public static void main(String[] args) {
+        Principal ventana = new Principal("Hola mundo AWT");
+    }
+}
